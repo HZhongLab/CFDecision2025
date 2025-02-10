a1c40a66093982343f084e4928780be291cedc9e
HZhongLab <91996112+HZhongLab@users.noreply.github.com>
2025-02-10 01:24:28 -0800

    Add files via upload

---
+++ Demo.m
@@ -0,0 +1,45 @@
+%Load data
+clear
+load('Demo.mat')
+mean_data = [];
+sem_data = [];
+
+%Calculate mean + sem
+dscore_FOV = dscore_FOV/0.5-1;
+mean_data = mean(dscore_FOV);
+sem_data = std(dscore_FOV)/sqrt(length(dscore_FOV));
+
+%Create empty figure
+scr=get(0,'ScreenSize');
+W=scr(3); H=scr(4);
+position=[300,200,225,300];
+default_font('Arial',16);
+Fig = figure('Position',position,...
+    'PaperUnits','points','PaperPosition',position,'color','w');
+
+% Plot data
+hold on
+color = [0.5 0 1;1 0.7529 0];
+
+plot(1:3, dscore_FOV,'-','Color',[0.8 0.8 0.8], 'LineWidth', 1.5, 'MarkerSize', 15);hold on
+errorbar(1:3, mean_data, sem_data, 'o-','Color','k',...
+'LineWidth', 2.5, 'MarkerSize', 10, 'MarkerEdgeColor', 'none', 'MarkerFaceColor', 'none');hold on
+
+yline(0,'--');
+alpha(0.2)
+xlim([0.5, 3+0.5])
+ylim([-0.2 0.8])
+yticks([0 0.4 0.8])
+xticks(1:3)
+xticklabels({'gen.', 'dis.','shuffled'})
+ylabel('Decoding score')
+set(gca,'linewidth',2)
+set(gca, 'TickDir', 'out')
+box off
+
+%Staitiscs (2-sample t test)
+[h1,p1] = ttest(dscore_FOV(:,1),dscore_FOV(:,2));
+[h2,p2] = ttest(dscore_FOV(:,2),dscore_FOV(:,3));
+disp(p1)
+disp(p2)
+
---
+++ README.m
@@ -0,0 +1,63 @@
+%% Overview
+% These codes are used for analyzing the data and reproducing the figures in Yiu et al.(2025)
+
+%% Systems Requirements
+
+%Hardware requirements:
+%These codes requires only a standard computer.
+
+%Software requirements:
+%These codes are supported for MATLAB in both windows and macOS. 
+%These codes has been tested on the following systems:
+%macOS: Sonoma (14.2.1)
+%MATLAB: R2023b
+
+%% Installation Guide
+
+%Please follow the standard steps from MathWorks documentation to install MATLAB. 
+%This process should normally take 20-30 minutues.
+%Link: https://www.mathworks.com/help/install/ug/install-products-with-internet-connection.html
+
+%% Demo 
+
+%To run the analysis and plot the data, just simply click "Run Section" button in
+%the MATLAB EDITOR. The code will automatically load the source data "Demo.mat" and the 
+%results (a line plot with a black thick line representing mean + sem and 
+%gray thin lines representing individual data points) will show up in a
+%seprate window. The expected run time is just a few seconds:
+
+Demo; 
+
+%% Instruction of use
+% To run the analysis and reproduce the plot of each panels, go to the
+% respective folder for each figure and select the master file (e.g.,
+% Figure1_master.m). Then, click "Run Section" button in
+% the MATLAB EDITOR for the figure panels that you would like to reproduce. 
+% The code will automatically load the source data and plot the 
+% results.
+
+%% License
+
+% MIT License
+% 
+% Copyright (c) [2025] [Tin-Long Yiu]
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
+
+
