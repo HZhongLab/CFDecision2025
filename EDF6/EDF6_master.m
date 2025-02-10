87d93706911d565816882c6d5ddb7f12aff77775
HZhongLab <91996112+HZhongLab@users.noreply.github.com>
2025-02-10 01:04:15 -0800

    Add files via upload

---
+++ EDF6/EDF6_master.m
@@ -0,0 +1,27 @@
+%% EDF6a
+% Neural trajectory for hit and low lick rate 8 kHz trials during discrimination
+close all
+EDF6a;
+
+%% EDF6b
+% Estimated difference in CF activity between high and low lick rate trials
+close all
+EDF6b;
+
+%% EDF6c
+% Similarity of high and low lick rate trajectories (dot product method)
+close all
+EDF6c;
+
+%% EDF6d
+% Estimated difference in CF activity between early and late lick onset trials
+close all
+EDF6d;
+
+
+%% EDF6e
+% Similarity of early and late lick onset trajectories (dot product method)
+close all
+EDF6e;
+
+
